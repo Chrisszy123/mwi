24ba79abc263219b3f601e4388718e0cf6358ab6
chrisszy123 <bassygoodluck@gmail.com>
2024-04-26 08:43:55 +0100

    fix: favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,6 +17,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Head>
+                {/* Replace 'favicon.ico' with the path to your custom favicon if necessary */}
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
       <body className={inter.className}>{children}</body>
     </html>
   );
